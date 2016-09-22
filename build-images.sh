4b082ab5cfd2e303d55ad1ba21763de148aa3ebe
tintoy <tintoy@tintoy.io>
2016-09-22 16:17:58 +1000

    Update Docker image naming.

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t ddresearch/terraform-ansible-deploy -t ddresearch/terraform-ansible-deploy:stable docker-images/terraform-ansible-deploy
-docker build -t ddresearch/tfa-template-multi-cloud -t ddresearch/tfa-template-multi-cloud:stable docker-images/tfa-template-multi-cloud
-docker build -t ddresearch/glider-gun-api -t ddresearch/glider-gun-api:stable docker-images/glider-gun-api
+docker build -t ddresearch/glider-gun-template-base-tfa -t ddresearch/glider-gun-template-base-tfa:stable docker-images/glider-gun-template-base-tfa
+docker build -t ddresearch/glider-gun-template-multi-cloud -t ddresearch/glider-gun-template-multi-cloud:stable docker-images/glider-gun-template-multi-cloud
+docker build -t ddresearch/glider-gun-api -t ddresearch/glider-gun-api:stable -f docker-images/glider-gun-api/Dockerfile .
