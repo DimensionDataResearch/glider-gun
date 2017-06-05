901aa284498003ab5f63981dc7e2f70b739189eb
Manish Kumar (ITaaS) <manish.p.kumar@itaas.dimensiondata.com>
2017-06-05 17:48:56 +1000

    Removing the client images from the repo

--- docker-images/glider-gun-template-multi-cloud/root/azure/web/resource-group.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "primary" {
-	name 					= "tfa-${var.prefix}-resources"
-	location 				= "${var.region_name}"
-}
--- docker-images/glider-gun-template-multi-cloud/root/azure/web/storage.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_storage_container" "primary" {
-	name 					= "tfa-${var.prefix}-vhds"
-	resource_group_name 	= "${var.storage_rg_name}"
-	storage_account_name 	= "${var.storage_acct_name}"
-	container_access_type 	= "private"
-}
